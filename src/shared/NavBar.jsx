3e5ce765f40bc1302f0c02459a7ad534647f82df
ryanr416 <ryanrichardson0993@gmail.com>
2023-09-25 09:36:54 -0400

    added cards

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -12,8 +12,8 @@ export default function NavBar() {
     <CardBody>
     <ButtonGroup gap='5'>
     <Button colorScheme='teal' as={Link} to="/home">Home</Button>
-    <Button as={Link} to="/about">About</Button>
     <Button as={Link} to="/portfolio">Portfolio</Button>
+    <Button as={Link} to="/about">About</Button>
     </ButtonGroup>
     </CardBody>
     </Card>
