a192df1be817f54890761df49a7397cd9ef27393
ryanr416 <ryanrichardson0993@gmail.com>
2023-09-28 12:45:43 -0400

    added about me animation

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,7 @@
 import { Link as ReactRouterLink } from 'react-router-dom'
-import { Card, CardHeader, CardBody, CardFooter, Text, Button } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Text, Button, Heading } from '@chakra-ui/react'
 import NavBar from '../../shared/NavBar'
-import "./About.css"
+import "../About/About.css"
 
 
 export default function About() {
@@ -10,15 +10,22 @@ return (
 
 
 
-<div>
+<div className="about">
 
 
 <NavBar />
-<Card>
-    <CardBody>
-    <Text>about.</Text>
-    </CardBody>
-    </Card>
+<div style={{ marginTop: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40vh' }}>
+<Card align='center' style={{ width: '900px'}} className="card-animation">
+  <CardHeader>
+    <Heading size='md'> A Little About Me</Heading>
+  </CardHeader>
+  <CardBody>
+    <Text> hello </Text>
+  </CardBody>
+  <CardFooter>
+  </CardFooter>
+</Card>
+</div>
 </div>
 )
 }
