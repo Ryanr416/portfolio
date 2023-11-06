17ee4f243875fae598003d798250e58fe8350ca5
ryanr416 <ryanrichardson0993@gmail.com>
2023-11-06 08:55:25 -0500

    added more cards

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ import {
   Divider,
   ButtonGroup,
   Flex,
-  CardFooter
+  CardFooter,
 } from '@chakra-ui/react';
 import NavBar from '../../shared/NavBar';
 import '../HomePage/HomePage.css';
@@ -20,9 +20,11 @@ export default function HomePage() {
       <NavBar />
       <div className="content-container">
         <div className="welcome-message">
+          <center>
           Welcome to Ryan's portfolio
-          <Flex justify="flex-start" mt="4rem" marginLeft="-33rem">
-            <Card maxW="sm" mr="2rem" maxH="800px" marginTop="10rem">
+          </center>
+          <Flex justify="center" align="center" mt="4rem">
+            <Card maxW="sm" maxH="800px" flex="1" margin="1rem">
               <div className="card-content">
                 <Image
                   src="https://ancient-voice-480.fly.dev/static/assets/images/hero_img.png"
@@ -40,22 +42,94 @@ export default function HomePage() {
                     {/* Add any additional content here */}
                   </Text>
                 </Stack>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <a href="https://ancient-voice-480.fly.dev/" target="_blank" rel="noopener noreferrer">
+                      <Button variant="solid" colorScheme="blue">
+                        View App Now
+                      </Button>
+                    </a>
+                    <a href="https://github.com/Dami-Olus/ga_capstone_prjct_wanderlust" target="_blank" rel="noopener noreferrer">
+                      <Button variant="solid" colorScheme="blue">
+                        View GitHub Now
+                      </Button>
+                    </a>
+                  </ButtonGroup>
+                </CardFooter>
               </div>
-              <Divider />
-              <CardFooter>
-                <ButtonGroup spacing="2">
-                  <a href="https://ancient-voice-480.fly.dev/" target="_blank" rel="noopener noreferrer">
-                    <Button variant="solid" colorScheme="blue">
-                      View App Now
-                    </Button>
-                  </a>
-                  <a href="https://github.com/Dami-Olus/ga_capstone_prjct_wanderlust" target="_blank" rel="noopener noreferrer">
-                    <Button variant="solid" colorScheme="blue">
-                      View GitHub Now
-                    </Button>
-                  </a>
-                </ButtonGroup>
-              </CardFooter>
+            </Card>
+
+            <Card maxW="sm" maxH="800px" flex="1" margin="1rem">
+              <div className="card-content">
+                <Image
+                  src="https://ancient-voice-480.fly.dev/static/assets/images/hero_img.png"
+                  borderRadius="lg"
+                />
+                <Stack mt="3" spacing="3">
+                  <Heading size="md">WanderLust</Heading>
+                  <Text fontSize="md">
+                    Wanderlust is an app that allows you to plan your itinerary for your next trip.
+                    You will be able to add a budget, add plans for a certain trip, and add friends to your planned
+                    trip. Organize your trip today with Wanderlust!
+                    Created with Python, Django, and GitHub
+                  </Text>
+                  <Text color="blue.600" fontSize="2xl">
+                    {/* Add any additional content here */}
+                  </Text>
+                </Stack>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <a href="https://ancient-voice-480.fly.dev/" target="_blank" rel="noopener noreferrer">
+                      <Button variant="solid" colorScheme="blue">
+                        View App Now
+                      </Button>
+                    </a>
+                    <a href="https://github.com/Dami-Olus/ga_capstone_prjct_wanderlust" target="_blank" rel="noopener noreferrer">
+                      <Button variant="solid" colorScheme="blue">
+                        View GitHub Now
+                      </Button>
+                    </a>
+                  </ButtonGroup>
+                </CardFooter>
+              </div>
+            </Card>
+
+            <Card maxW="sm" maxH="800px" flex="1" margin="1rem">
+              <div className="card-content">
+                <Image
+                  src="https://ancient-voice-480.fly.dev/static/assets/images/hero_img.png"
+                  borderRadius="lg"
+                />
+                <Stack mt="3" spacing="3">
+                  <Heading size="md">WanderLust</Heading>
+                  <Text fontSize="md">
+                    Wanderlust is an app that allows you to plan your itinerary for your next trip.
+                    You will be able to add a budget, add plans for a certain trip, and add friends to your planned
+                    trip. Organize your trip today with Wanderlust!
+                    Created with Python, Django, and GitHub
+                  </Text>
+                  <Text color="blue.600" fontSize="2xl">
+                    {/* Add any additional content here */}
+                  </Text>
+                </Stack>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <a href="https://ancient-voice-480.fly.dev/" target="_blank" rel="noopener noreferrer">
+                      <Button variant="solid" colorScheme="blue">
+                        View App Now
+                      </Button>
+                    </a>
+                    <a href="https://github.com/Dami-Olus/ga_capstone_prjct_wanderlust" target="_blank" rel="noopener noreferrer">
+                      <Button variant="solid" colorScheme="blue">
+                        View GitHub Now
+                      </Button>
+                    </a>
+                  </ButtonGroup>
+                </CardFooter>
+              </div>
             </Card>
           </Flex>
         </div>
@@ -66,5 +140,3 @@ export default function HomePage() {
     </div>
   );
 }
-
-  
